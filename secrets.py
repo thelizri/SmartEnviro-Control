9dbd4e145b9e6306400d96b330a9b3d064f9f529
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-06-30 21:43:56 +0200

    Update

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# Wireless network
+WIFI_SSID = "Replace"
+WIFI_PASS = "Replace"
+
+# Adafruit IO (AIO) configuration
+AIO_SERVER = "Replace"
+AIO_PORT = 1883
+AIO_USER = "Replace"
+AIO_KEY = "Replace"
+AIO_LIGHTS_FEED = "Replace"
+AIO_TEMPERATURE_FEED = "Replace"
+AIO_HUMIDITY_FEED = "Replace"
