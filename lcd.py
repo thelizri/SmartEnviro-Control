1384e190d46c5bb8ae6ba79e413f809fc21cb26b
Karlan <50805319+thelizri@users.noreply.github.com>
2023-07-05 06:12:27 +0200

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -1,12 +1,11 @@
-import machine
-from machine import I2C
+from machine import I2C, Pin
 from pico_i2c_lcd import I2cLcd
 
 I2C_ADDR     = 0x27
 I2C_NUM_ROWS = 4
 I2C_NUM_COLS = 20
 
-i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 lcd.clear()
 lcd.putstr("Starting")
